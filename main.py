d14a77814e303c4c656135caa57028dcc792b458
Vipul <vipul.agg27@gmail.com>
2017-04-01 15:26:13 +0530

    gangster bot ready

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from chatterBotApi.python.chatterbotapi import ChatterBotFactory, ChatterBotType
-
+from gizoogleTranslateApi.GizoogleTranslator import GizoogleTranslator
 factory = ChatterBotFactory()
 
 #bot1 = factory.create(ChatterBotType.CLEVERBOT)
@@ -7,12 +7,20 @@ factory = ChatterBotFactory()
 
 bot2 = factory.create(ChatterBotType.PANDORABOTS, 'b0dafd24ee35a477')
 bot2session = bot2.create_session()
+gizoogleTranslator = GizoogleTranslator()
 
 inputText = raw_input('Say something to Bot: ')
 while (1):
 	
     #print 'bot1> ' + s
 	botReply = bot2session.think(inputText)
-	print "bot> " + botReply
+	print "bot> " + gizoogleTranslator.translate(botReply)
 	inputText = raw_input("You> ")
     #s = bot1session.think(s);
+
+
+
+
+#print gizoogleTranslator.translate(translateInput)
+
+#print(response.text)
