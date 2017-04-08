0a3010c33d1305817d88184f9dc16a39d36726a4
Vipul Aggarwal <vipaggar@adobe.com>
2017-04-08 15:47:04 +0530

    fb webhook established

--- web/gangasterbot/facebookMessenger/views.py
+++ web/gangasterbot/facebookMessenger/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
-
+import json
 # Create your views here.
 from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. This is Faacebook Messenger API.")
+    #print request
+    #req=json.loads(request.body)
+    if request.method == 'POST':
+        return
+    else:
+        if request.GET.get('hub.verify_token') == 'make_me_an_offer_i_can_not_refuse':
+            return HttpResponse(request.GET.get('hub.challenge'))
+        else:
+           return HttpResponse("Hello, world. This is Faacebook Messenger API.")
--- web/gangasterbot/obj/Release/Package/PackageTmp/facebookMessenger/views.py
+++ web/gangasterbot/obj/Release/Package/PackageTmp/facebookMessenger/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
-
+import json
 # Create your views here.
 from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. This is Faacebook Messenger API.")
+    #print request
+    #req=json.loads(request.body)
+    if request.method == 'POST':
+        return
+        # What should I return here???
+    else:
+        #req=json.loads(request.GET)
+        #return HttpResponse("Hello, world. This is Faacebook Messenger API.")
+        if request.GET.get('hub.verify_token') == 'make_me_an_offer_i_can_not_refuse':
+            #print request.GET;
+            return HttpResponse(request.GET.get('hub.challenge'))
+            #res.status(200).send(req.query['hub.challenge']);
+        else:
+            #console.error("Failed validation. Make sure the validation tokens match.");
+           return HttpResponse("Hello, world. This is Faacebook Messenger API.")
